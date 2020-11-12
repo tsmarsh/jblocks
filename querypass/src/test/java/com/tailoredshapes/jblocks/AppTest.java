ff0e359eea3d7d14c9eb8a089b4f1ce9fed8cd95
Tom Marsh <ts.marsh@gmail.com>
2020-11-12 06:43:41 -0500

    QueryPass is a little more trust worthy

--- querypass/src/test/java/com/tailoredshapes/jblocks/AppTest.java
+++ querypass/src/test/java/com/tailoredshapes/jblocks/AppTest.java
@@ -1,33 +1,34 @@
 package com.tailoredshapes.jblocks;
 
-import static com.tailoredshapes.underbar.IO.slurp;
-import static io.restassured.RestAssured.given;
-import static org.junit.Assert.assertTrue;
-import static spark.Service.ignite;
-
-import org.json.simple.JSONObject;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import spark.Service;
 
-import java.io.InputStream;
+import static com.tailoredshapes.underbar.IO.slurp;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static spark.Service.ignite;
 
 public class AppTest {
 
     private static App app;
 
-    static Service testServer;
+    private static Service testServer;
     private static int testPort = 7070;
     private static int port = 8067;
+    private static String receivedBody;
 
 
     @BeforeClass
     public static void setUp() throws Exception {
         testServer = ignite();
         testServer.port(testPort);
-        testServer.post("/", (req, resp) -> req.body());
-
+        testServer.post("/", (req, resp) -> {
+            receivedBody = req.body();
+            return "bounced";
+        });
 
 
         app = new App(
@@ -44,10 +45,15 @@ public class AppTest {
     }
 
     @Test
-    public void canQueryAGoodJSON(){
+    public void canQueryAGoodJSON() {
         var good = slurp(getClass().getResourceAsStream("/good.json"));
+        var expected = slurp(getClass().getResourceAsStream(("/expected.json")));
+
         given().port(port).body(good).
                 when().post("/").
-                then().statusCode(200);
+                then()
+                .body(equalTo("[\"bounced\"]"))
+                .statusCode(200);
+        assertEquals(expected, receivedBody);
     }
 }
